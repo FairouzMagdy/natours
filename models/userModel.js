5363f79c61a0119b6e5b72125bf78ec25a75d969
fairouzmagdy <fairouzmagdy219@gmail.com>
2024-03-13 01:16:58 +0200

    Implemented password hashing using bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please tell us your name'],
-    unique: true,
     minLength: [5, 'A user name must have at least 5 characters'],
   },
   email: {
@@ -15,7 +15,15 @@ const userSchema = mongoose.Schema({
     lowercase: true,
     validate: [validator.isEmail, 'Please provide a valid email.'],
   },
-  photo: String,
+  photo: {
+    type: String,
+    default: 'default.jpg',
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
   password: {
     type: String,
     required: [true, 'Please provide a password'],
@@ -24,7 +32,24 @@ const userSchema = mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // Only works on CREATE or SAVE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete PasswordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
