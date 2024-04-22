0646f0c4782e324cdc65c74d86a8c2b027eb0ac7
fairouzmagdy <fairouzmagdy219@gmail.com>
2024-04-22 17:48:16 +0200

    Sending JWT via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,14 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true, // Prevents browsers from manipulating the cookie (prevents xss attacks)
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
 
   res.status(statusCode).json({
     status: 'success',
