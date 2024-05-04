c0d512c6276de8f38e0c29b38b1a637cd4119ee9
fairouzmagdy <fairouzmagdy219@gmail.com>
2024-05-05 02:18:54 +0300

    Integrated stripe

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const hpp = require('hpp');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
+
 const chatRouter = require('./routes/chatRoutes');
 const messageRouter = require('./routes/messageRoutes');
 
@@ -72,6 +74,8 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
+
 app.use('/api/v1/chats', chatRouter);
 app.use('/api/v1/messages', messageRouter);
 
