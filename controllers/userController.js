de1c07750a71b3007a59709fed20aab58801ba79
fairouzmagdy <fairouzmagdy219@gmail.com>
2024-05-09 00:06:22 +0300

    Upload multiple images for tours

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,10 @@
+const multer = require('multer');
+const sharp = require('sharp');
+
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
-const multer = require('multer');
-const sharp = require('sharp');
 
 // const multerStorage = multer.diskStorage({
 //   destination: (req, file, cb) => {
@@ -32,19 +33,19 @@ const upload = multer({
 
 exports.uploadUserPhoto = upload.single('photo');
 
-exports.resizeUserPhoto = (req, res, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
     .toFile(`public/img/users/${req.file.filename}`);
 
   next();
-};
+});
 
 const filterObj = (obj, ...includedFields) => {
   const newObj = {};
