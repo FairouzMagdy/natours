2abbaf9873ed99db041242c8c55929c9f3c3931d
fairouzmagdy <fairouzmagdy219@gmail.com>
2024-05-11 21:30:09 +0300

    fixed issue with resetting password with code

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,7 +9,7 @@ const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const sendEmail = require('./../utils/email');
 
-const signToken = (id) => {
+const signToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
