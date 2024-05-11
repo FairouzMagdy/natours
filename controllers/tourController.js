233fe518f7158a560ba8185378b3b2cf8bb864c0
fairouzmagdy <fairouzmagdy219@gmail.com>
2024-05-11 21:35:09 +0300

    removed commented code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -150,51 +150,6 @@ exports.monthlyPlan = catchAsync(async (req, res, next) => {
 
 // /tours-within/:distance/center/:latlng/unit/:unit
 // /tours-within/233/center/34.111745,-118.113491/unit/mi
-
-// exports.getDistances = catchAsync(async (req, res, next) => {
-//   const { latlng, unit } = req.params;
-//   const [lat, lng] = latlng.split(',');
-
-//   const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
-
-//   if (!lat || !lng) {
-//     next(
-//       new AppError(
-//         'Please provide latitude and longitude in the format lat,lng.',
-//         400,
-//       ),
-//     );
-//   }
-
-//   const distances = await Tour.aggregate([
-//     {
-//       $geoNear: {
-//         near: {
-//           type: 'Point',
-//           coordinates: [lng * 1, lat * 1],
-//         },
-//         distanceField: 'distance',
-//         distanceMultiplier: multiplier,
-//       },
-//     },
-//     {
-//       $project: {
-//         distance: 1,
-//         name: 1,
-//       },
-//     },
-//   ]);
-
-//   res.status(200).json({
-//     status: 'success',
-//     data: {
-//       data: distances,
-//     },
-//   });
-// });
-
-// /tours-within/:distance/center/:latlng/unit/:unit
-// /tours-within/233/center/34.111745,-118.113491/unit/mi
 exports.getToursWithin = catchAsync(async (req, res, next) => {
   const { distance, latlng, unit } = req.params;
   const [lat, lng] = latlng.split(',');
